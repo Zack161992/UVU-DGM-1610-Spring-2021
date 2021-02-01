389832143bf8c0fafb40d229333d4b4975cc5807
zack young <zack.e.y16@gmaill.com>
2021-01-31 20:48:34 -0700

    Added Movement Script And Camera Follow

---
+++ Prototype 1/Assets/Script/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Access Modifier, Data Type, Name
+    public float speed = 5.0f;
+
+public float hInput;
+public float fInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput); 
+      transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+      
+    }
+}
