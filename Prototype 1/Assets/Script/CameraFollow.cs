c1e5722a6a45ffabbdd722c7839fc43e4884ba28
zack young <zack.e.y16@gmaill.com>
2021-01-31 22:14:27 -0700

    Completed Prototype 1

--- Prototype 1/Assets/Script/CameraFollow.cs
+++ Prototype 1/Assets/Script/CameraFollow.cs
@@ -6,12 +6,7 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3 (0,7,-11);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
--- Prototype 1/Assets/Script/Move.cs
+++ Prototype 1/Assets/Script/Move.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     // Access Modifier, Data Type, Name
-    public float speed = 5.0f;
+    private float speed = 30.0f;
+    private float turnSpeed = 35.0f;
 
-public float hInput;
-public float fInput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+private float hInput;
+private float fInput;
 
     // Update is called once per frame
     void Update()
     {
+        //Gathers the inputs for the controls
         hInput = Input.GetAxis("Horizontal");
         fInput = Input.GetAxis("Vertical");
+        // Makes the vehicle go forward and back
       transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput); 
-      transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
+      // Makes the vehicle go left and right
+      transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
       
     }
 }
