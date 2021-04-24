74fe5c6f71bb08378e9cd45596f85e7847b3e306
zack young <zack.e.y16@gmaill.com>
2021-04-24 17:32:39 -0600

    cleaned up code on my personl project and made comments

--- Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/FollowPlayer.cs
+++ Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 2, -4);
+    private Vector3 offset = new Vector3(0, 2, -4);  // Setting exact positioning of the camera
 
     // Start is called before the first frame update
     void Start()
--- Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/PlayerController.cs
+++ Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     // Access Modifier, Data Type, Name
     private float speed = 30.0f;
-    private float turnSpeed = 35.0f;
+    private float turnSpeed = 80.0f;
     private float zBound = 60;
     public float xRange = 60;
-
     private float hInput;
     private float fInput;
 
@@ -21,23 +20,18 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+        // Prevents the player from leaving the top, bottom, left and right of the map
     {
-        //Gathers the inputs for the controls
-        hInput = Input.GetAxis("Horizontal");
-        fInput = Input.GetAxis("Vertical");
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);  // Makes the Player go forward and back
-
-        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime); // Makes the Player go left and right
-
         if (transform.position.z < -zBound)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);  // Made constraints on the edge of map barrier on the top of the map
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);  // Made constraints on the edge of map barrier on z axis the top of the map
         }
+        
         if (transform.position.z > zBound)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);  // Made constraints on the edge of map barrier on the bottom of the map
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);  // Made constraints on the edge of map barrier on the z axis bottom of the map
         }
+
 
         if (transform.position.x < -xRange)
         {
@@ -48,5 +42,15 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z); // Made constraints on the edge of map barrier on x axis
         }
 
+        //Moves the player based on arrow key input
+        {
+            hInput = Input.GetAxis("Horizontal");  //Gathers the inputs for the controls
+            fInput = Input.GetAxis("Vertical");   //Gathers the inputs for the controls
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);  // Makes the Player go forward and back
+
+            transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime); // Makes the Player go left and right
+        }
     }
+
 }
