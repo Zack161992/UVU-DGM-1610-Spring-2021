fd4f869a4001a210dd48d1595098a68069f8b95b
zack young <zack.e.y16@gmaill.com>
2021-04-25 03:07:47 -0600

    finished prototype 4

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPerfab;
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPerfab, GenerateSpawnPosition(), powerupPerfab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPerfab, GenerateSpawnPosition(), powerupPerfab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++) // creates multiple enemies
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
