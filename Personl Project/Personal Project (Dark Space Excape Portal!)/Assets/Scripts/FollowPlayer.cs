2702a4800572814fea8ffde8455bf30aaa52101e
zack young <zack.e.y16@gmaill.com>
2021-04-30 22:07:59 -0600

    personal project

--- Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/FollowPlayer.cs
+++ Personl Project/Personal Project (Dark Space Excape Portal!)/Assets/Scripts/FollowPlayer.cs
@@ -16,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset; // The position of the player 
     }
 }
