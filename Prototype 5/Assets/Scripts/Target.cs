5b580cf67315e99c1ca6dbe8a858eb6e02c7124a
zack young <zack.e.y16@gmaill.com>
2021-04-30 23:12:59 -0600

    Prototype 5

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -2;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse); // Adding Force in the up direction
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);// Adding Torque to get rotation of object
+
+        transform.position = RandomSpawnPos(); // putting the object randomly somewhere under where the eye cant see
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager. isGameActive)
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void OnTriggerEnter(Collider other) // collides with the sensor bar
+    {
+        Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);// calculating random force
+    }
+
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);// random Torque
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);//random position
+    }
+}
+
